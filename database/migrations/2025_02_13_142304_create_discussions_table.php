a161704fb361fcbec80976c5ac0e52e355f68c41
apppiiipp <hafifsyahputra2506@gmail.com>
2025-02-13 21:24:12 +0700

    create table

--- database/migrations/2025_02_13_142304_create_discussions_table.php
+++ database/migrations/2025_02_13_142304_create_discussions_table.php
@@ -11,16 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('discussions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',["student", "lecturer"]);
-            $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -29,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('discussions');
     }
 };
